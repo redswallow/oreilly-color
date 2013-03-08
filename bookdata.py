f2cc48f3009a79d8da2410197a8ea508b8498c34
redswallow <redswallowjysc@gmail.com>
2013-03-08 15:00:35 +0800

    visualize book cliques

--- bookdata.py
+++ bookdata.py
@@ -2,6 +2,8 @@ import pickle,BeautifulSoup,urllib2,re
 import numpy as np
 import pylab as P
 import nltk
+import networkx as nx
+import matplotlib.pyplot as plt
 
 RAW_FILE='bookdata_raw'
 PICKLE_FILE='bookdata_pickle'
@@ -18,7 +20,7 @@ def get_books():
         with open(RAW_FILE,'r') as f:
             for line in f.readlines():
                 book={}
-                book['color'],book['name'],book['animal']=line.split(TAB)
+                book['color'],book['name'],book['animal']=line.split(TAB) #lower title
                 book['color']=' '.join(book['color'].split()) # fix: remove the last space in color name 'PMS 301C '
                 books.append(book)
 
@@ -78,6 +80,27 @@ def book_keywords(books):
                 wordset[word]=1
     return sorted(wordset.items(), key=lambda d: d[1],reverse=True)
 
+def network(books):
+    stopwords=get_stopwords()
+    G=nx.Graph()
+    for book1 in books:
+        G.add_node(book1['name'])
+        book1_words=book1['name'].lower().split()
+        for book2 in books:
+            if book1==book2:
+                continue
+            else:
+                for word in book1_words:
+                    book2_words=book2['name'].lower().split()
+                    if word not in stopwords and word in book2_words:
+                        G.add_edge(book1['name'],book2['name'])
+    pos=nx.spring_layout(G)
+    nx.draw_networkx_edges(G,pos,alpha=0.05)
+    nx.draw_networkx_nodes(G,pos,node_size=[G.degree(v)*10+10 for v in G],alpha=0.3)
+    #nx.draw_networkx_labels(G,pos,alpha=0.3)
+    print G.degree()
+    plt.show()
+
 def wordle(words):
     tags = make_tags(fdist.items()[:100], maxsize=50)
     create_tag_image(tags, 'sample_word_cloud.png', size=(900, 600), background=(0, 0, 0))
@@ -87,4 +110,5 @@ books=get_books()
 colors=color_set(books)
 #book_num_by_colors_img(colors)
 #print colors['PMS Reflex Blue']
-print book_keywords(colors['PMS 2725C'])
+#print book_keywords(colors['PMS 301C'])
+network(books)
